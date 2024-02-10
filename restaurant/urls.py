5cc5a0a4a2041300d8f44fb0f4c3455fa3c1833a
Ali Itani <aliitani0@gmail.com>
2024-02-10 16:44:40 -0500

    Views and Model Set up

--- restaurant/urls.py
+++ restaurant/urls.py
@@ -1,7 +1,9 @@
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='index')
+    path('', views.index, name='index'),
+    path('items/', views.MenuItemsView.as_view()),
+    path('items/<int:pk>', views.SingleMenuItemView.as_view())
 ]
